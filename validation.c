76bf229ea886dc29c4f3b6b7bd7cec7731523c92
Omar Tarek <omar.t4352@gmail.com>
2021-08-28 22:17:18 +0200

    Validation final .v3

--- validation.c
+++ validation.c
@@ -1,18 +1,25 @@
 #include "types.h"
 #include "validation.h"
 #include "DB.h"
-
+#include <stdlib.h>
+extern uint32_t g_current_pan ;
 EN_transStat_t validate_terminal_data(ST_cardData_t* cardData, ST_terminalData_t* terminalData)
 {
-    uint32_t c_month = 0, c_year=0, t_month=0, t_year=0;
-    c_month = cardData->cardExpirationData[0] * 10 + cardData->cardExpirationData[1];
-    c_year = cardData->cardExpirationData[3] * 10 + cardData->cardExpirationData[4];
-    t_month = terminalData->transactionDate[3]*10 + terminalData->transactionDate[4];
-    t_year = terminalData->transactionDate[8]*10 + terminalData->transactionDate[9];
 
-    if(c_month >= t_month)
+    uint32_t c_month = 0, c_year=0, t_month=0, t_year=0;
+    c_month = atoi(cardData->cardExpirationData[0]) * 10 + atoi(cardData->cardExpirationData[1]);
+    c_year = atoi(cardData->cardExpirationData[3]) * 10 + atoi(cardData->cardExpirationData[4]);
+    t_month = atoi(terminalData->transactionDate[3]) * 10 + atoi(terminalData->transactionDate[4]);
+    t_year = atoi(terminalData->transactionDate[8] * 10) + atoi(terminalData->transactionDate[9]);
+
+   // printf ("Inside validate terminal data function \n") ;
+    printf ("card month = %d \n" , c_month ) ;
+    printf ("card year = %d \n" , c_year ) ;
+    printf ("terminal month = %d \n" , t_month ) ;
+    printf ("terminal year = %d \n" , t_year ) ;
+    if((c_month >= t_month) && (c_year >= t_year))
     {
-        if(terminalData->transAmount <= terminalData->maxTransAmount)
+        if((terminalData->transAmount) <= (terminalData->maxTransAmount))
         {
             return APPROVED;
         }
@@ -30,10 +37,10 @@ EN_transStat_t validate_terminal_data(ST_cardData_t* cardData, ST_terminalData_t
 }
 EN_transStat_t validate_server (ST_cardData_t* cardData, ST_terminalData_t* terminalData , database_t* server)
 {
-    uint32_t account_idx = Is_PAN_exist(cardData->primaryAccountNumber);
+    uint32_t account_idx = Is_PAN_exist(g_current_pan);
     if(account_idx != -1)
     {
-        if(terminalData->transAmount <= server[account_idx].balance)
+        if((terminalData->transAmount) <= (server[account_idx].balance))
         {
             return APPROVED;
         }
@@ -47,3 +54,4 @@ EN_transStat_t validate_server (ST_cardData_t* cardData, ST_terminalData_t* term
         return DECLINED;
     }
 }
+
