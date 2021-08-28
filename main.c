81df6595e390b76613cc2ae1fa24980608d2b550
Mahmoud Ayoub <84600209+Mahmoud-Ayoub@users.noreply.github.com>
2021-08-28 22:02:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,27 +6,37 @@
 #include "stdint.h"
 #include "types.h"     // for structures' definitions
 #include "input.h"
+#include "DB.h"
+#include "transinfo.h"
+#include "validation.h"
+
 /*
  * NOTES : must include new header files which includes the each function prototype
  *
  */
+ST_cardData_t card ;
+ST_terminalData_t terminal ;
+
 
 ST_cardData_t* p_card = NULL ;
 
 ST_terminalData_t* p_terminal = NULL ;
 
-ST_transaction_t trans_data = {card , terminal , DECLINED} ;
+extern database_t *db;
+uint32_t g_current_pan ;
+
+ST_transaction_t trans_data ;
 ST_transaction_t* p_trans_data = &trans_data ;
 
-ST_cardData_t* get_card_data (void) ;      /* Access to the global pointer p_card */
-ST_terminalData_t* get_terminal_data (void) ;	 /* Access to the global pointer p_terminal */
+//ST_cardData_t* get_card_data (void) ;      /* Access to the global pointer p_card */
+//ST_terminalData_t* get_terminal_data (void) ;	 /* Access to the global pointer p_terminal */
 
-int validate_terminal_data (ST_terminalData_t * p_t) ;  /* (check expiration + amount) returns (0 >> failed) or (1 >> success)  */
-EN_transStat_t validate_server (ST_transaction_t* p_trans) ; 	/* returns an enum status  */
-void save_transaction (ST_transaction_t* p_trans) ; 			/* save transaction in server */
+//EN_transStat_t validate_terminal_data(ST_cardData_t* cardData, ST_terminalData_t* terminalData);  /* (check expiration + amount) returns (0 >> failed) or (1 >> success)  */
+//EN_transStat_t validate_server (ST_cardData_t* cardData, ST_terminalData_t* terminalData , database_t* server) ;  	/* returns an enum status  */
+//void save_transaction (ST_transaction_t* p_trans) ; 			/* save transaction in server */
 
 void app (void) ;
-void view_history (void) ;
+//void view_history (void) ;
 void approved (void) ;
 void rejected (void) ;
 
@@ -40,10 +50,10 @@ int main () {
 		printf ("choice = %d \n" , choice) ;
 		switch (choice) {
 		case 1 :
-			//app () ;
+			app () ;
 			break ;
 		case 2 :
-			//view_history() ;
+			view_history() ;
 			break ;
 		}
 	}
@@ -51,21 +61,32 @@ int main () {
 }
 
 void app (void) {
+	create_database();
+	init_database(db);
 	ST_cardData_t*  card_Data_ptr = get_card_data() ;  // get Card Data   (sure it is valid)
 	p_card = card_Data_ptr;
 	ST_terminalData_t* terminal_Data_ptr =  get_terminal_data() ;   // get Terminal Data  (sure it is valid)
 	p_terminal =  terminal_Data_ptr;
-	
-	if (validate_terminal_data(p_terminal) == 0) {   // invalid Terminal Data (expired OR too much amount)
+
+	printf ("Card and Terminal data are collected \n") ;
+	printf ("name = %s \n" , p_card->cardHolderName) ;
+	printf ("expiry data = %s \n" , p_card->cardExpirationData) ;
+	printf ("account number = %s \n" , p_card->primaryAccountNumber) ;
+
+	if (validate_terminal_data(p_card , p_terminal) == DECLINED) {   // invalid Terminal Data (expired OR too much amount)
+		printf ("rejected in terminal data validation \n") ;
 		rejected() ;
 	}
 	else {                               // valid Terminal Data (Card Not expired + Not too much amount)
-		if (validate_server(p_trans_data) == DECLINED) {  // data doesn't match with server data
+		if (validate_server(p_card , p_terminal , db) == DECLINED) {  // data doesn't match with server data
+			printf ("rejected in server validation \n") ;
 			rejected() ;
 		}
-		else if (validate_server(p_trans_data) == APPROVED) {   // data matches with server data
+		else if (validate_server(p_card , p_terminal , db) == APPROVED) {   // data matches with server data
+			printf ("inside else if in main \n") ;
 			approved() ;
-			save_transaction(p_trans_data) ;
+			trans_data.cardHolderData = card ;
+			save_transaction(p_card , p_terminal) ;
 		}
 	}
 }
