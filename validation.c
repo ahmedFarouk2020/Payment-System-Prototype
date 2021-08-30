b03bebcec8c6da2e746eb3268cc9f06ba9074471
Mahmoud Ayoub <84600209+Mahmoud-Ayoub@users.noreply.github.com>
2021-08-30 15:01:16 +0200

    Update validation.c

--- validation.c
+++ validation.c
@@ -13,11 +13,6 @@ EN_transStat_t validate_terminal_data(ST_cardData_t* cardData, ST_terminalData_t
     t_month = ((terminalData->transactionDate[3])-'0') * 10 + ((terminalData->transactionDate[4]-'0'));
     t_year = ((terminalData->transactionDate[8]-'0') * 10) + ((terminalData->transactionDate[9]-'0'));
 
-   // printf ("Inside validate terminal data function \n") ;
-    printf ("card month = %d \n" ,c_month ) ;
-    printf ("card year = %d \n" , c_year ) ;
-    printf ("terminal month = %d \n" , t_month ) ;
-    printf ("terminal year = %d \n" , t_year ) ;
     if((c_month >= t_month) && (c_year >= t_year))
     {
         if((terminalData->transAmount) <= (terminalData->maxTransAmount))
@@ -38,7 +33,7 @@ EN_transStat_t validate_terminal_data(ST_cardData_t* cardData, ST_terminalData_t
 }
 EN_transStat_t validate_server (ST_cardData_t* cardData, ST_terminalData_t* terminalData , database_t* server)
 {
-    uint32_t account_idx = Is_PAN_exist(g_current_pan);
+    int32_t account_idx = Is_PAN_exist(g_current_pan);
     if(account_idx != -1)
     {
         if((terminalData->transAmount) <= (server[account_idx].balance))
